6dec66cf60a0a19b5b3266b738951214e7b9e42c
Cerebra Doria <cdoria@at-k8.msk.21-school.ru>
2022-06-13 20:47:52 +0300

    added some structs

---
+++ includes/structs.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdoria <cdoria@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 19:24:45 by cdoria            #+#    #+#             */
+/*   Updated: 2022/06/13 20:46:51 by cdoria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+typedef	struct s_list
+{
+	void					*key;
+	void					*value;
+	typedef	struct s_list	*next;
+}							t_list;
+
+typedef struct s_buildins_ptr
+{
+	/* data */
+}		t_buildins_ptr;
+
+typedef struct s_info
+{
+	char				**envp;
+	t_list				envp_list;
+	t_buildins_ptr		buildins[7];
+	char				commands[7];
+	char				envp_f;
+	char				exif_f;
+	int					status;
+}						t_info;
+
+#endif
