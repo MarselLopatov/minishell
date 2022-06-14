2abd8280b277655340a43bf2779e26b57f31700e
Chen Oleta <coleta@at-l7.msk.21-school.ru>
2022-06-14 19:09:41 +0300

    change error .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,15 @@
 /*   By: coleta <coleta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 18:39:36 by cdoria            #+#    #+#             */
-/*   Updated: 2022/06/14 19:05:26 by coleta           ###   ########.fr       */
+/*   Updated: 2022/06/14 19:07:31 by coleta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "include.h"
+# include "structs.h"
 
 void	init(char **enpv);
 
