e01895815537397c034714a46ea7cb5b8faef50d
Chen Oleta <coleta@at-c1.msk.21-school.ru>
2022-06-22 18:33:00 +0300

    fixed ft_unset

---  builtins/ft_unset.c
+++  builtins/ft_unset.c
@@ -1,10 +1,43 @@
 #include "../includes/minishell.h"
 
+void	*ft_realloc(void *old, size_t new_size)
+{
+	void	*new;
+	int		i;
+
+	new = malloc(new_size);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (i < new_size)
+	{
+		((char *)new)[i] = ((char *)old)[i];
+		i++;
+	}
+	free(old);
+	return (new);
+}
+
+static void	up_mass(int k)
+{
+	while (info.envp[k + 1])
+	{
+		info.envp[k] = info.envp[k + 1];
+		k++;
+		if (!info.envp[k + 1])
+		{
+			free(info.envp[k]);
+			info.envp[k] = NULL;
+		}
+	}
+	info.envp = ft_realloc(info.envp, (k - 1) * sizeof(char *));
+}
+
 void ft_unset(char **args)
 {
-	int	i;
-	int	j;
-	int	k;
+	int		i;
+	int		j;
+	char	*temp;
 
 	i = 0;
 	while (args[i])
@@ -14,14 +47,10 @@ void ft_unset(char **args)
 		{
 			if (args[i] == info.envp[j])
 			{
-				k = j;
-				while (info.envp[k])
-				{
-					if (info.envp[k + 1])
-						info.envp[k] = info.envp[k + 1];
-					k++;
-				}
-				free(info.envp[k - 1]);
+				temp = info.envp[j];
+				info.envp[j] = NULL;
+				up_mass(j);
+				free(temp);
 				
 			}	
 			j++;
